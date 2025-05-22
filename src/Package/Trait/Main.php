6c2351a8280c9e4300529cd3e19a7bae40442853
remco van der Velde <remco.pc@outlook.com>
2025-05-22 08:54:42 +0200

    added search package

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -62,171 +62,175 @@ trait Main {
         $object = $this->object();
         $source = $object->config('controller.dir.data') . 'Search' . $object->config('extension.json');
         $data = $object->data_read($source);
-        $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', $options->url, [
-        'verify' => false,  // Disable SSL certificate verification (localhost)
-        ]);
-        $html = $res->getBody();
 
-        $doc = new DOMDocument();
-        libxml_use_internal_errors(true);
-        $doc->loadHTML($html);
-        libxml_clear_errors();
+        if(!is_array($options->url)){
+            $options->url = [$options->url];
+        }
+        foreach($options->url as $url){
+            $client = new GuzzleHttp\Client();
+            $res = $client->request('GET', $url, [
+                'verify' => false,  // Disable SSL certificate verification (localhost)
+            ]);
+            $html = $res->getBody();
 
-        // Get plain text content
-        $body = $doc->getElementsByTagName('body')->item(0);
-        $plain_text = $body->textContent;
-        $plain_text = str_replace(
-            [
-                "\r\n",
-                "\n\r",
-                "\r",
-            ],
-            [
-                "\n",
-                "\n",
-                "\n",
-            ],
-            $plain_text
-        );
-        $list = explode(PHP_EOL, $plain_text);
-        $paragraph_nr = 0;
-        $paragraph = [];
-        foreach($list as $nr => $line){
-            $line = trim($line);
-            if (empty($line)) {
-                $paragraph_nr++;
-                continue;
+            $doc = new DOMDocument();
+            libxml_use_internal_errors(true);
+            $doc->loadHTML($html);
+            libxml_clear_errors();
+
+            // Get plain text content
+            $body = $doc->getElementsByTagName('body')->item(0);
+            $plain_text = $body->textContent;
+            $plain_text = str_replace(
+                [
+                    "\r\n",
+                    "\n\r",
+                    "\r",
+                ],
+                [
+                    "\n",
+                    "\n",
+                    "\n",
+                ],
+                $plain_text
+            );
+            $list = explode(PHP_EOL, $plain_text);
+            $paragraph_nr = 0;
+            $paragraph = [];
+            foreach($list as $nr => $line){
+                $line = trim($line);
+                if (empty($line)) {
+                    $paragraph_nr++;
+                    continue;
+                }
+                if(!array_key_exists($paragraph_nr, $paragraph)){
+                    $paragraph[$paragraph_nr] = [];
+                }
+                $paragraph[$paragraph_nr][] = $line;
             }
-            if(!array_key_exists($paragraph_nr, $paragraph)){
-                $paragraph[$paragraph_nr] = [];
+            $paragraph = array_values($paragraph);
+            if($data){
+                $document_list = $data->get('document') ?? [];
+                $id_document = $data->get('id.document') ?? 0;
+                $id_document++;
+                $paragraph_list = $data->get('paragraph') ?? [];
+                $id_paragraph = $data->get('id.paragraph') ?? 0;
+                $id_paragraph++;
+                $word_list = $data->get('word') ?? [];
+                $id_word = $data->get('id.word') ?? 0;
+                $id_word++;
+                $sentence_list = $data->get('sentence') ?? [];
+                $id_sentence = $data->get('id.sentence') ?? 0;
+                $id_sentence++;
+            } else {
+                $document_list = [];
+                $id_document = 1;
+                $paragraph_list = [];
+                $id_paragraph = 1;
+                $word_list = [];
+                $id_word = 1;
+                $sentence_list = [];
+                $id_sentence = 1;
+                $data = new Data();
             }
-            $paragraph[$paragraph_nr][] = $line;
-        }
-        $paragraph = array_values($paragraph);
-        if($data){
-            $document_list = $data->get('document') ?? [];
-            $id_document = $data->get('id.document') ?? 0;
-            $id_document++;
-            $paragraph_list = $data->get('paragraph') ?? [];
-            $id_paragraph = $data->get('id.paragraph') ?? 0;
-            $id_paragraph++;
-            $word_list = $data->get('word') ?? [];
-            $id_word = $data->get('id.word') ?? 0;
-            $id_word++;
-            $sentence_list = $data->get('sentence') ?? [];
-            $id_sentence = $data->get('id.sentence') ?? 0;
-            $id_sentence++;
-        } else {
-            $document_list = [];
-            $id_document = 1;
-            $paragraph_list = [];
-            $id_paragraph = 1;
-            $word_list = [];
-            $id_word = 1;
-            $sentence_list = [];
-            $id_sentence = 1;
-            $data = new Data();
-        }
-        $document_list_nr = null;
-        if($document_list){
-            foreach($document_list as $document_list_nr => $document_list_item){
-                if($document_list_item->url === $options->url){
-                    $id_document = $document_list_item->id;
-                    break;
+            $document_list_nr = null;
+            if($document_list){
+                foreach($document_list as $document_list_nr => $document_list_item){
+                    if($document_list_item->url === $options->url){
+                        $id_document = $document_list_item->id;
+                        break;
+                    }
                 }
             }
-        }
-        $document = (object) [
-            'id' => $id_document,
-            'url' => $options->url,
-            'paragraph' => []
-        ];
-        foreach($paragraph as $paragraph_nr => $lines){
-            $sentence_paragraph_list = [];
-            foreach($lines as $line){
-                $word_line = explode(' ', $line);
-                $sentence = (object) [
-                    'id' => $id_sentence,
-                    'word' => [],
-                    'text' => ''
-                ];
-                $found = false;
-                foreach($word_line as $word_line_nr => $word){
+            $document = (object) [
+                'id' => $id_document,
+                'url' => $options->url,
+                'paragraph' => []
+            ];
+            foreach($paragraph as $paragraph_nr => $lines){
+                $sentence_paragraph_list = [];
+                foreach($lines as $line){
+                    $word_line = explode(' ', $line);
+                    $sentence = (object) [
+                        'id' => $id_sentence,
+                        'word' => [],
+                        'text' => ''
+                    ];
                     $found = false;
-                    foreach($word_list as $word_list_nr => $word_list_item){
-                        if($word_list_item->word === $word){
+                    foreach($word_line as $word_line_nr => $word){
+                        $found = false;
+                        foreach($word_list as $word_list_nr => $word_list_item){
+                            if($word_list_item->word === $word){
+                                $found = true;
+                                $sentence->word[] = $word_list_item->id;
+                                $sentence->text .= $word_list_item->word . ' ';
+                                break;
+                            }
+                        }
+                        if(!$found){
+                            $word_list[] = (object) [
+                                'id' => $id_word,
+                                'word' => $word
+                            ];
+                            $sentence->word[] = $id_word;
+                            $sentence->text .= $word . ' ';
+                            $id_word++;
+                        }
+                    }
+                    if(!$found){
+                        $sentence->text = substr($sentence->text, 0, -1);
+                    }
+                    $sentence->text = rtrim($sentence->text);
+                    $found = false;
+                    foreach($sentence_list as $sentence_list_nr => $sentence_list_item){
+                        if($sentence_list_item->text === $sentence->text){
                             $found = true;
-                            $sentence->word[] = $word_list_item->id;
-                            $sentence->text .= $word_list_item->word . ' ';
+                            $sentence = $sentence_list_item;
                             break;
                         }
                     }
                     if(!$found){
-                        $word_list[] = (object) [
-                            'id' => $id_word,
-                            'word' => $word
-                        ];
-                        $sentence->word[] = $id_word;
-                        $sentence->text .= $word . ' ';
-                        $id_word++;
+                        $sentence_list[] = $sentence;
+                        $id_sentence++;
                     }
+                    $sentence_paragraph_list[] = $sentence;
                 }
-                if(!$found){
-                    $sentence->text = substr($sentence->text, 0, -1);
-                }
-                $sentence->text = rtrim($sentence->text);
                 $found = false;
-                foreach($sentence_list as $sentence_list_nr => $sentence_list_item){
-                    if($sentence_list_item->text === $sentence->text){
+                foreach($paragraph_list as $paragraph_list_nr => $paragraph_list_item){
+                    if($paragraph_list_item->sentence === $sentence_paragraph_list){
                         $found = true;
-                        $sentence = $sentence_list_item;
+                        $paragraph = $paragraph_list_item;
                         break;
                     }
                 }
                 if(!$found){
-                    $sentence_list[] = $sentence;
-                    $id_sentence++;
-                }
-                $sentence_paragraph_list[] = $sentence;
-            }
-            $found = false;
-            foreach($paragraph_list as $paragraph_list_nr => $paragraph_list_item){
-                if($paragraph_list_item->sentence === $sentence_paragraph_list){
-                    $found = true;
-                    $paragraph = $paragraph_list_item;
-                    break;
+                    $paragraph_list[] = (object) [
+                        'id' => $id_paragraph,
+                        'sentence' => $sentence_paragraph_list
+                    ];
+                    $document->paragraph[] = $id_paragraph;
+                    $id_paragraph++;
+                } else {
+                    $document->paragraph[] = $paragraph->id;
                 }
             }
-            if(!$found){
-                $paragraph_list[] = (object) [
-                    'id' => $id_paragraph,
-                    'sentence' => $sentence_paragraph_list
-                ];
-                $document->paragraph[] = $id_paragraph;
-                $id_paragraph++;
+            if($document_list_nr !== null){
+                $document_list[$document_list_nr] = $document;
             } else {
-                $document->paragraph[] = $paragraph->id;
+                $document_list[] = $document;
             }
+            $data->set('document', $document_list);
+            $data->set('id.document', $id_document);
+            $data->set('paragraph', $paragraph_list);
+            $data->set('id.paragraph', $id_paragraph);
+            $data->set('sentence', $sentence_list);
+            $data->set('id.sentence', $id_sentence);
+            $data->set('word', $word_list);
+            $data->set('id.word', $id_word);
         }
-        if($document_list_nr !== null){
-            $document_list[$document_list_nr] = $document;
-        } else {
-            $document_list[] = $document;
-        }
-        $data->set('document', $document_list);
-        $data->set('id.document', $id_document);
-        $data->set('paragraph', $paragraph_list);
-        $data->set('id.paragraph', $id_paragraph);
-        $data->set('sentence', $sentence_list);
-        $data->set('id.sentence', $id_sentence);
-        $data->set('word', $word_list);
-        $data->set('id.word', $id_word);
         d($data);
         d($source);
         ddd($options);
-
-
     }
 }
 
