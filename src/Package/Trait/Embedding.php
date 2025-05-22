781d23237a6f27840bac3af4d5ee52132c1924b9
remco van der Velde <remco.pc@outlook.com>
2025-05-22 09:45:01 +0200

    added search package

--- src/Package/Trait/Embedding.php
+++ src/Package/Trait/Embedding.php
@@ -30,9 +30,34 @@ trait Embedding {
             return;
         }
         foreach($words as $word){
+            $word = $this->get_embedding($word);
             ddd($word);
         }
 
     }
+
+    public function get_embedding($word){
+        if(!is_object($word)){
+            return $word;
+        }
+        if(!property_exists('word', $word)){
+            return $word;
+        }
+        $text = $word->word;
+        /*
+        curl http://localhost:11434/api/embed -d '{
+            "model": "all-minilm",
+            "input": "Why is the sky blue?"
+
+        }'
+        */
+        $command = 'curl http://localhost:11434/api/embed -d \'{
+            "model": "nomic-embed-text",
+            "input": "' . $text . '"
+        }\'';
+        $output = shell_exec($command);
+        ddd($output);
+    }
 }
+
 
