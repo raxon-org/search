2d46058dddf31851f6cc422675c64ee9694d52bc
remco van der Velde <remco.pc@outlook.com>
2025-05-22 21:47:00 +0200

    added search package

--- src/Package/Trait/Find.php
+++ src/Package/Trait/Find.php
@@ -93,7 +93,6 @@ trait Find {
                     }
                 }
                 if(array_key_exists($embedding->id, $list)){
-                    ddd($list[$embedding->id] ?? []);
                     $sentence = $list[$embedding->id]->sentence ?? [];
                     if(empty($sentence)){
                         foreach($sentences as $sentence_id => $sentence_data){
