6c62b32a8548788329e2953b9f7f0beb3f869b26
remco van der Velde <remco.pc@outlook.com>
2025-05-24 10:43:45 +0200

    init

--- src/Package/Trait/Embedding.php
+++ src/Package/Trait/Embedding.php
@@ -645,6 +645,7 @@ trait Embedding {
         foreach($embeddings as $embedding){
             foreach($embedding->embedding as $nr => $id_float){
                 if($is_debug){
+                    breakpoint($floats);
                     d($floats[$id_float]);
                     ddd($id_float);
                 }
