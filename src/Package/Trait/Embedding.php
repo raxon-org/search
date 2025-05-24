0363e71bfd7b336bdf73b7ddc03f22ff271ba715
remco van der Velde <remco.pc@outlook.com>
2025-05-24 11:07:09 +0200

    init

--- src/Package/Trait/Embedding.php
+++ src/Package/Trait/Embedding.php
@@ -270,13 +270,6 @@ trait Embedding {
                 'sentence' => $sentence_piece->sentence
             ];
             $sentence_piece->hash = hash('sha256', Core::object($hash, Core::JSON_LINE));
-            d($sentence_piece->hash);
-            d($sentence_pieces_hashes);
-            breakpoint(in_array(
-                $sentence_piece->hash,
-                $sentence_pieces_hashes,
-                true
-            ));
             if(
                 !in_array(
                     $sentence_piece->hash,
