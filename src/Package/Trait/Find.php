f7f3c2589c63940ae148a70d80e247bc36f868e3
remco van der Velde <remco.pc@outlook.com>
2025-05-24 18:46:35 +0200

    init

--- src/Package/Trait/Find.php
+++ src/Package/Trait/Find.php
@@ -127,6 +127,7 @@ trait Find {
             if(is_array($vector) && is_array($embedding_sentence_piece->embedding)) {
                 $similarity = [];
                 foreach($embedding_sentence_piece->embedding as $nr => $embedding){
+                    ddd($embedding);
                     $embedding = $this->get_embedding_float($embedding, $floats);
                     $similarity[] = $this->cosine_similarity($vector, $embedding);
                 }
