0cca3f905b1bc39bc2243165ff157c3a008b0341
remco van der Velde <remco.pc@outlook.com>
2025-05-25 02:35:43 +0200

    init

--- src/Package/Trait/Embedding.php
+++ src/Package/Trait/Embedding.php
@@ -79,7 +79,7 @@ trait Embedding {
         ]];
         */
         $count_words = $data->get('id.embedding.word') ?? 0;
-        ddd($count_words);
+        ddd($data->get('íd'));
         $count = 0;
         foreach($words as $word){
             if(property_exists($word, 'word') && $word->word === ''){
