c09b23771c1823037f5aeb0271eb73e1a5bf9bdb
remco van der Velde <remco.pc@outlook.com>
2025-06-02 21:14:43 +0200

    init

--- src/Package/Trait/Find.php
+++ src/Package/Trait/Find.php
@@ -115,10 +115,10 @@ trait Find {
             $data_embedding_sentence_piece = $object->data_read($source_embedding_sentence_piece);
             $key = $offset + 1;
             $shmop = SharedMemory::open($key, 'a', 0, 0);
-//            $shmop = false;
+            $shmop = false;
             if($shmop){
                 $size = File::size($source_embedding_word);
-                $part_size = ((1024 * 1024) * 8);
+                $part_size = ((1024 * 1024) * 4);
                 $parts = ceil($size / $part_size);
                 $read = [];
                 for($i = 0; $i < $parts; $i++){
@@ -156,7 +156,7 @@ trait Find {
                 $read = File::read($source_embedding_word);
 //                $gzip = gzencode($read, 9);
                 $size = File::size($source_embedding_word);
-                $part_size = ((1024 * 1024) * 8);
+                $part_size = ((1024 * 1024) * 4);
                 $parts = ceil($size / $part_size);
                 $split = mb_str_split($read, $part_size);
                 for($i = 0; $i < $parts; $i++){
