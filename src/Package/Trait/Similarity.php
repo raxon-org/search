5b2b7c02ffae487c07f57d75ad24cc59fdc29fd0
remco van der Velde <remco.pc@outlook.com>
2025-06-04 17:02:58 +0200

    init

--- src/Package/Trait/Similarity.php
+++ src/Package/Trait/Similarity.php
@@ -154,14 +154,17 @@ trait Similarity {
                                 $size_read += $part_size;
                             }
                         }
-                        $duration = microtime(true) - $object->config('time.start');
-                        if($i === 0){
-                            $i = 1;
+                        if($i % 10 === 0){
+                            //every 40 MB
+                            $duration = microtime(true) - $object->config('time.start');
+                            if($i === 0){
+                                $i = 1;
+                            }
+                            $speed = round(($size_read / $duration) * 8, 3); //byte -> bit
+                            $duration_percentage = round($duration / ($i / $parts), 3);
+                            $duration_left = round($duration_percentage - $duration, 3);
+                            echo Cli::tput('cursor.up') . Cli::tput('erase.line') . 'Memory read: ' . File::size_format($size_read). '; percentage: ' . round(($i / $parts) * 100, 3) . '; time left: ' . $duration_left . ' sec; duration: ' . round($duration, 3) . ' sec; speed:' . str_replace('B', 'bit', File::size_format($speed)) . '/sec;' . PHP_EOL;
                         }
-                        $speed = round(($size_read / $duration) * 8, 3); //byte -> bit
-                        $duration_percentage = round($duration / ($i / $parts), 3);
-                        $duration_left = round($duration_percentage - $duration, 3);
-                        echo Cli::tput('cursor.up') . Cli::tput('erase.line') . 'Memory read: ' . File::size_format($size_read). '; percentage: ' . round(($i / $parts) * 100, 3) . '; time left: ' . $duration_left . ' sec; duration: ' . round($duration, 3) . ' sec; speed:' . str_replace('B', 'bit', File::size_format($speed)) . '/sec;' . PHP_EOL;
                     }
                     echo Cli::tput('cursor.up') . Cli::tput('erase.line') . 'Memory read: ' . File::size_format($size_read). '; percentage: ' . 100 . '; time left: ' . 0 . ' sec; duration: ' . round($duration, 3) . ' sec;' .PHP_EOL;
                     $start = microtime(true);
