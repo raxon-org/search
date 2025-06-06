f7a727fb6394bbeeff8d0a17b57d09b05006f8a6
remco van der Velde <remco.pc@outlook.com>
2025-06-06 04:07:45 +0200

    init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -422,7 +422,7 @@ trait Main {
                 $html[] = '<p>' . $plain_text . '</p>';
                 $html[] = '</body>';
                 $html[] = '</html>';
-                $target_url = $target_dir . str_replace(['/'], ['_'], $title_text) . hash('sha256', $plain_text);
+                $target_url = $target_dir . str_replace(['/'], ['_'], $title_text) . hash('sha256', $plain_text) . $object->config('extension.html');
                 File::write($target_url, implode(PHP_EOL, $html));
                 File::permission($object, ['url' => $target_url]);
 
