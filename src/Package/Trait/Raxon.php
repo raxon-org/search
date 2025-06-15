f316b8daafe36247281d75d707bfa6da95074bf3
remco van der Velde <remco.pc@outlook.com>
2025-06-15 22:42:45 +0200

    xp

--- src/Package/Trait/Raxon.php
+++ src/Package/Trait/Raxon.php
@@ -194,7 +194,7 @@ trait Raxon {
         $source = $dir_version . 'Search' . $object->config('extension.json');
         $dir = new Dir();
         $read = $dir->read($options->source);
-        $partition = Core::array_partition($read, 500); // (100 * 1 GB ? )
+        $partition = Core::array_partition($read, 2); // (100 * 1 GB ? )
         $total = count($partition);
         $count = 0;
         foreach($partition as $nr => $chunk){
