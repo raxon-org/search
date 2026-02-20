834360c0a28375570504e25d772e7d0475f4124e
Remco van der Velde <remco@universeorange.com>
2026-02-20 08:38:30 +0100

    k

--- src/Package/Trait/Find.php
+++ src/Package/Trait/Find.php
@@ -125,6 +125,7 @@ trait Find {
                     $part .= $key_to_char[$model[$i]] ?? '';
                     break;
                 }
+                ddd($part);
                 if(array_key_exists($part, $result)){
                     $result[$part]++;
 //                    $result_header[$part][] = Core::object($header, Core::JSON_LINE);
