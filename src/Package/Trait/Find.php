d3998d0f2779c8c6bd351444d46dedf957d15a38
Remco van der Velde <remco@universeorange.com>
2026-02-20 10:51:07 +0100

    k

--- src/Package/Trait/Find.php
+++ src/Package/Trait/Find.php
@@ -160,7 +160,8 @@ trait Find {
             }
             elseif(
                 $pointer_max > 0 &&
-                $nr > $pointer_max
+                $nr > $pointer_max &&
+                $pointer_max > $pointer_min * 1.02      //minimal growth of 2%
             ){
                 break;
             }
