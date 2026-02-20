07efbf4ff3d93657679390e98551f314c0207295
Remco van der Velde <remco@universeorange.com>
2026-02-20 06:42:20 +0100

    k

--- src/Package/Trait/Find.php
+++ src/Package/Trait/Find.php
@@ -95,7 +95,7 @@ trait Find {
                     $line .= $key_to_char[$model[$i]] ?? '';
                 }
                 $part = '';
-                for($i = $nr + 1 + $search_count; $i <= $end; $i++){
+                for($i = $nr + $search_count; $i <= $end; $i++){
                     $part .= $key_to_char[$model[$i]] ?? '';
                 }
                 //line whole line nr...
