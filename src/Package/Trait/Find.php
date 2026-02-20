18215e5a43b375d82da8aaa0b2e3ad8d3ea4d5ca
Remco van der Velde <remco@universeorange.com>
2026-02-20 07:49:28 +0100

    k

--- src/Package/Trait/Find.php
+++ src/Package/Trait/Find.php
@@ -111,8 +111,9 @@ trait Find {
                     $line .= $key_to_char[$model[$i]] ?? '';
                 }
                 $part = '';
-                for($i = $nr + $search_count; $i <= $i+1; $i++){
+                for($i = $nr + $search_count; $i <= $end; $i++){
                     $part .= $key_to_char[$model[$i]] ?? '';
+                    break;
                 }
                 if(array_key_exists($part, $result)){
                     $result[$part]++;
