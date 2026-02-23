0fed386a1b60dd2f5aa7b92159ec031a5420cc60
Remco van der Velde <remco@universeorange.com>
2026-02-23 16:44:28 +0100

    k

--- src/Package/Trait/Service.php
+++ src/Package/Trait/Service.php
@@ -260,6 +260,28 @@ trait Service {
                                 $pos[] = strpos($next_token_token, '?');
                                 $pos[] = strpos($next_token_token, '!');
                                 $pos[] = strpos($next_token_token, ':');
+                                $pos[] = strpos($next_token_token, '(');
+                                $pos[] = strpos($next_token_token, ')');
+                                $pos[] = strpos($next_token_token, '{');
+                                $pos[] = strpos($next_token_token, '}');
+                                $pos[] = strpos($next_token_token, '|');;
+                                $pos[] = strpos($next_token_token, '\\');
+                                $pos[] = strpos($next_token_token, '//');
+                                $pos[] = strpos($next_token_token, '<');
+                                $pos[] = strpos($next_token_token, '>');
+                                $pos[] = strpos($next_token_token, '-');
+                                $pos[] = strpos($next_token_token, '_');
+                                $pos[] = strpos($next_token_token, '+');
+                                $pos[] = strpos($next_token_token, '=');
+                                $pos[] = strpos($next_token_token, '`');
+                                $pos[] = strpos($next_token_token, '~');
+                                $pos[] = strpos($next_token_token, '@');
+                                $pos[] = strpos($next_token_token, '#');
+                                $pos[] = strpos($next_token_token, '$');
+                                $pos[] = strpos($next_token_token, '%');
+                                $pos[] = strpos($next_token_token, '^');
+                                $pos[] = strpos($next_token_token, '&');
+                                $pos[] = strpos($next_token_token, '*');
                                 foreach($pos as $key => $value){
                                     if($value === false){
                                         unset($pos[$key]);
