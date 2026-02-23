a038a41b1ba22fbc78bdf9332eabaf77a25ca5ad
Remco van der Velde <remco@universeorange.com>
2026-02-23 12:23:56 +0100

    k

--- src/Package/Trait/Service.php
+++ src/Package/Trait/Service.php
@@ -250,7 +250,7 @@ trait Service {
                                 }
                                 $next_token_token = $next_token->token;
                                 $partition_enable = $next_token->partition->enable ?? null;
-                                d($partition_enable);
+                                d($next_token);
                                 $explode = explode(' ', $next_token_token, 2);
                                 if(array_key_exists(1, $explode)){
                                     if($explode[0] !== ' '){
