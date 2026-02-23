fba396c8667ef70c7b0ea13bdc3a0e99806f654d
Remco van der Velde <remco@universeorange.com>
2026-02-23 17:41:41 +0100

    k

--- src/Package/Trait/Service.php
+++ src/Package/Trait/Service.php
@@ -243,6 +243,7 @@ trait Service {
                             break;
                         case 'word':
                             $next_word = '';
+                            $partition_enable = null;
                             $next_word = $this->word_next($partition, $file, $partition_enable, $this->search($search, $char_to_key));
                             $ask = $file->node->ask;
                             $partition_enable = null;
