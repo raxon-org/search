2ad43fa2e998acc53aa25315c1c25eaed266f2bf
Remco van der Velde <remco@universeorange.com>
2026-06-25 23:52:48 +0200

    init

--- src/Package/Trait/Source.php
+++ src/Package/Trait/Source.php
@@ -176,7 +176,7 @@ trait Source {
             $chunk_length = 0;
             foreach($file->chunks as $chunk_nr => $file_chunk){
                 foreach($file_chunk as $nr => $line){
-                    if($nr < 10 && array_key_exists(0, $chunk, true)){
+                    if($nr < 10 && array_key_exists(0, $chunk)){
                         $chunk[] = $line;
                     }
                     elseif($nr === 10){
